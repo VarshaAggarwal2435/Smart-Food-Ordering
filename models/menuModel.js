5b725be44f99fce3a0e19c350a4915e1f309e97e
Varsha Aggarwal <va2435160@gmail.com>
2024-11-28 14:22:42 +0530

    Images uploaded

--- models/menuModel.js
+++ models/menuModel.js
@@ -3,6 +3,10 @@ const mongoose = require("mongoose");
 const menuSchema = mongoose.Schema({
     name:String,
     price:Number,
+    image: {
+        type: String, // Stores the file path or URL
+        required: true, // Ensure every item has an image
+    },
 });
 
 module.exports = mongoose.model("menu", menuSchema);
